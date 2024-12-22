7a54d859e5a21a754ee4cf2a329241378d7531f5
StefanyAnahiMoralesSanchez <anahisp.morales@gmail.com>
2024-12-22 16:39:32 -0500

    Add files via upload

---
+++ #Proyecto Final.py
@@ -0,0 +1,56 @@
+#Proyecto Final
+#Declaro palabras de las cuidades de Ecuador
+a=(str)
+# pantalla 1
+print('Bienvenido al juego del AHORCADO')
+    # Colocar imagen referente al juego
+# pantalla 2 
+print('REGISTRESE')
+print('Nombres completos:')
+b=(str)
+if(a==range(10)):
+    print('ERROR:Ingrese solo letras')
+print('Nombre de jugador:')
+c=(str)
+x=int(input('Ingresa tu edad:  '))
+print({x})
+if(x<15):
+    print('No puedes continuar')
+    exit()
+# pantalla 3
+y=int(input('Selecciona un nivel de juego:  '))
+z=1
+w=2
+v=3
+if(y==z):
+    print('Selecionaste el nivel facil')
+    if(y==w):
+        print('Seleccionaste el nivel medio')
+        if(y==v):
+            print('Selecionaste el nivel dificil')
+else:
+    (4<y>0)
+    print('Nivel seleccionado no existe, intente otra vez')
+# pantalla 4
+print('CUIDADES DEL ECUADOR')
+print('Presione ENTER para inicar el juego')
+    # Colocar imagen referida
+print('Despliegue de letras')
+tupla = ("A","B","C","CH","D","E","F","G","H","I","J","K","L","LL","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+print(tupla)
+print("Selecciona una letra")
+letra_acierto = ("A")
+letra = str(input("""
+Adivina la letra y acierta !!!
+Ingresa una letra entre A y Z: """))
+for i in ("A","L","A","U","S","I"):
+            while letra != letra_acierto:
+                letra = str(input("Ingresa otra letra:"))
+    # pantalla 4.1 
+print('FELICIDADES palabra completada')
+    # pantalla 4.2
+print('GANASTE ESTRELLAS')
+    # Colocar cantidad de estrellas
+# pantalla 5
+print('Deseas continuar')
+    # Colocar imagen de SI - NO
